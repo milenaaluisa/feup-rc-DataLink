7333e6a22cc04929b512b4abd59c55fe5e572b49
Milena Gouveia <up202008862@g.uporto.pt>
2022-10-21 15:00:53 +0100

    Small changes in data_transfer() according to new definition of assemble_supervision_frame()

--- transmitter.c
+++ transmitter.c
@@ -61,13 +61,13 @@ int stop_transmission(int fd) {
     return 1;
 }
 
-/* commented for now because of changes to function assemble_information_frame()
 int data_transfer(int fd, char* data, int num_packets) {
     int ns = 0, nr;
     int num_successful_packets = 0;
     int reply_from_receiver;
     char control_field = assemble_info_frame_ctrl_field (ns);
-    char* info_frame = assemble_information_frame(control_field, data);
+    char* info_frame = malloc(INFO_FRAME_SIZE);
+    int info_frame_size = assemble_information_frame(control_field, data, info_frame);
 
     while (num_successful_packets < num_packets) {
         (void) signal(SIGALRM, alarm_handler);
@@ -75,7 +75,7 @@ int data_transfer(int fd, char* data, int num_packets) {
 
         while (alarm_count < 3) {
             if (!alarm_enabled) {
-                write(fd, info_frame, INFO_FRAME_SIZE);
+                write(fd, info_frame, info_frame_size);
                 printf("Information frame sent\n");
                 alarm(3);
                 alarm_enabled = 1;
@@ -83,12 +83,14 @@ int data_transfer(int fd, char* data, int num_packets) {
 
             if (!state_machine(fd)) { 
                 nr = control_rcv & BIT(7);
+
                 if (control_rcv & RR_ACK == RR_ACK && nr != ns) {
                     num_successful_packets++;
                     data += DATA_FIELD_BYTES;
                     ns = (ns == 0) ? 1 : 0;
+                    
                     control_field = assemble_info_frame_ctrl_field(ns);
-                    info_frame =  assemble_information_frame(control_field, data);
+                    info_frame_size = assemble_information_frame(control_field, data, info_frame);
                 }
                 reply_from_receiver = 1;
                 break;
@@ -101,7 +103,7 @@ int data_transfer(int fd, char* data, int num_packets) {
         }
     }
     return 0;
-} */
+}
 
 int main(int argc, char *argv[]) {
     const char *serialPortName = argv[1];
