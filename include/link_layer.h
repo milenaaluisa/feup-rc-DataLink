9d2403df07237e137d197b46ccc7e9a091d0a9a9
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-10-22 10:47:35 +0100

    Implemented llclose and added start_transmission error verification

--- include/link_layer.h
+++ include/link_layer.h
@@ -74,8 +74,7 @@ int llwrite(const unsigned char *buf, int bufSize);
 int llread(unsigned char *packet);
 
 // Close previously opened connection.
-// if showStatistics == TRUE, link layer should print statistics in the console on close.
 // Return "1" on success or "-1" on error.
-int llclose(int showStatistics);
+int llclose(int fd, LinkLayer connection_parameters);
 
 #endif // _LINK_LAYER_H_
--- src/link_layer.c
+++ src/link_layer.c
@@ -10,10 +10,10 @@ int llopen(LinkLayer connection_parameters) {
     if (create_termios_structure(fd, connection_parameters.serial_port)) 
         return -1;
 
-    if (connection_parameters.role == LlRx)
-        rx_start_transmission(fd);
-    else
-        tx_start_transmission(fd);
+    if (connection_parameters.role == LlRx && rx_start_transmission(fd)) 
+        return -1;
+    else if (connection_parameters.role == LlTx && tx_start_transmission(fd))
+        return -1;
     return fd;
 }
 
@@ -27,7 +27,10 @@ int llread(unsigned char *packet) {
     return 0;
 }
 
-int llclose(int showStatistics) {
-    // TODO
-    return 1;
+int llclose(int fd, LinkLayer connection_parameters) {
+    if (connection_parameters.role == LlRx && rx_stop_transmission(fd))
+        return -1;
+    else if (connection_parameters.role == LlTx && tx_stop_transmission(fd))
+        return -1;
+    return 0;
 }
